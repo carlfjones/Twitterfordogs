c6cea2d62db2ea256c0a7cb29adf696203943164
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-02-12 15:15:18 +0000

    Create woof objects in client.js

--- client/client.js
+++ client/client.js
@@ -1,1 +1,17 @@
 console.log("Hello World")
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content')
+
+    const woof = {
+        name,
+        content
+    }
+    console.log(woof);
+
+});
